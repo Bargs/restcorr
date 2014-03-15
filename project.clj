d8e418fab168e5b2eaf47facb0bfe3fa75765589
mbargar <mbargar@gmail.com>
2014-03-14 21:44:25 -0400

    adding dependencies and starting core.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject restcorr "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Experiments with Clojure and REST"
+  :url "https://github.com/Bargs/restcorr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [liberator "0.11.0"]
+                 [compojure "1.1.6"]
+                 [ring/ring-core "1.2.2"]
+                 [ring/ring-jetty-adapter "1.2.2"]])
