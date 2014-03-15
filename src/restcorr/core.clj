366c6f30ccce7d59f2f2fc6c1a9f6122f385bc60
mbargar <mbargar@gmail.com>
2014-03-14 22:25:30 -0400

    some html response

--- src/restcorr/core.clj
+++ src/restcorr/core.clj
@@ -1,11 +1,12 @@
-(ns core
+(ns restcorr.core
   (:require [liberator.core :refer [resource defresource]]
             [ring.middleware.params :refer [wrap-params]]
             [ring.adapter.jetty :refer [run-jetty]]
             [compojure.core :refer [defroutes ANY]]))
 
 (defroutes app
-  (ANY "/" [] (resource)))
+  (ANY "/foo" [] (resource :available-media-types ["text/html"]
+                           :handle-ok "<html>Hello, Internet.</html>")))
 
 (def handler
   (-> app
