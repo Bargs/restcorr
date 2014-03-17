ab9cb0e159eb4f8d5f761476b070e9e798c0c6a1
mbargar <mbargar@gmail.com>
2014-03-16 23:45:07 -0400

    adding some tests

--- project.clj
+++ project.clj
@@ -11,5 +11,7 @@
                  [korma "0.3.0-RC6"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [postgresql/postgresql "8.4-702.jdbc4"]]
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]
+                                  [cheshire "5.3.1"]]}}
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler restcorr.core/handler})
--- test/restcorr/core_test.clj
+++ test/restcorr/core_test.clj
@@ -1,7 +1,18 @@
 (ns restcorr.core-test
   (:require [clojure.test :refer :all]
-            [restcorr.core :refer :all]))
+            [restcorr.core :refer :all]
+            [ring.mock.request :refer :all]
+            [cheshire.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest get-author-test
+  (is (= (handler (request :get "/authors/1212"))
+         {:status 200
+          :headers {"Vary" "Accept"
+                    "Content-Type" "application/json;charset=UTF-8"}
+          :body "{\"first_name\":\"John\",\"last_name\":\"Worsley\",\"id\":1212}"})))
+
+(deftest get-authors-test
+  (let [response (handler (request :get "/authors"))
+        body (parse-string (:body response) true)]
+    (is (= (:status response) 200))
+    (is (= (count body) 19))))
